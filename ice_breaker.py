d21fb7cf5812cb8512987dee3e3a238dca5266fd
jackyjlxing <xingjl@hotmail.com>
2024-10-08 22:40:19 -0400

    linkedIn

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
 
 from third_parties.linkedin import scrape_linkedin_profile
 
@@ -19,11 +21,12 @@ if __name__ == "__main__":
         input_variables=["information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
+    # llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
+    llm = ChatOllama(model = "llama3" )
 
     chain = summary_prompt_template | llm
     linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
+        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/", mock=True
     )
     res = chain.invoke(input={"information": linkedin_data})
 
